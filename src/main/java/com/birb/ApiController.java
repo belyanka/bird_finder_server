b69396b61d7241101ee067fa3c610194daf567dc
belyanka <belyankamew@gmail.com>
2017-02-11 19:51:42 +0300

    получение json из таблицы

--- src/main/java/com/birb/ApiController.java
+++ src/main/java/com/birb/ApiController.java
@@ -1,16 +1,26 @@
 package com.birb;
 
+import com.birb.domain.BirdDescriptionEntity;
+import com.birb.repositories.BirdDescriptionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @RestController
 public class ApiController {
 
+    @Autowired
+    private BirdDescriptionRepository repository;
+
     @RequestMapping(value = "/bird", method = RequestMethod.GET)
-    public String index() {
-        return "Greetings from Spring Boot!";
+    public List<BirdDescriptionEntity> bird() {
+        Iterable<BirdDescriptionEntity> birds = repository.findAll();
+        return new ArrayList<>((Collection<? extends BirdDescriptionEntity>) birds);
     }
 
 }
---
+++ src/main/java/com/birb/repositories/BirdDescriptionRepository.java
@@ -0,0 +1,10 @@
+package com.birb.repositories;
+
+import com.birb.domain.BirdDescriptionEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface BirdDescriptionRepository extends CrudRepository<BirdDescriptionEntity, Long> {
+
+}
