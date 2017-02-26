061ed6a7338eb4d31d5dab3a2aa9ca10bf2951b6
belyanka <belyankamew@gmail.com>
2017-02-26 21:18:49 +0300

    table 2

---
+++ src/main/java/com/birb/Config.java
@@ -0,0 +1,35 @@
+package com.birb;
+
+public class Config {
+
+    //BODY TYPE
+    public static int BODY_TYPE_ORDINARY_ROUND = 1;
+    public static int BODY_TYPE_ORDINARY_LONG = 2;
+    public static int BODY_TYPE_DUCK = 3;
+    public static int BODY_TYPE_CRANE = 4;
+    public static int BODY_TYPE_VYP = 5;
+    public static int BODY_TYPE_CARNIVORE = 6;
+    public static int BODY_TYPE_OWL = 7;
+
+    //BEAK TYPE
+    public static int BEAK_TYPE_STRAIGHT = 1;
+    public static int BEAK_TYPE_DUCK = 2;
+    public static int BEAK_TYPE_CURVED = 3;
+    public static int BEAK_TYPE_CARNIVORE = 4;
+
+    //LEG TYPE
+    public static int LEG_TYPE_DUCK = 1;
+    public static int LEG_TYPE_POG = 2;
+    public static int LEG_TYPE_ORDINARY = 3;
+    public static int LEG_TYPE_CARNIVORE = 4;
+
+    //TAIL TYPE
+    public static int TAIL_TYPE_ROUND = 1;
+    public static int TAIL_TYPE_SQUARE = 2;
+    public static int TAIL_TYPE_HALVED = 3;
+    public static int TAIL_TYPE_TET = 4;
+    public static int TAIL_TYPE_PIKE = 5;
+    public static int TAIL_TYPE_LUSH = 6;
+
+
+}
